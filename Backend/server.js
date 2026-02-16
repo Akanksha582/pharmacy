c03e95c611e1f7ee08f2f48b797d14c842b7bdfc
Akanksha582 <akanksham582@gmail.com>
2026-02-16 11:12:21 +0530

    add mongoDb connection

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import productRoutes from "./routes/product.route.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
+
+
+// MongoDB connect
+mongoose.connect("mongodb://127.0.0.1:27017/pharmacyDB")
+.then(()=> console.log("✅ MongoDB Connected"))
+.catch(err => console.log(err));
+
+// test route
+app.get("/", (req,res)=>{
+  res.send("🚀 Pharmacy Backend Running");
+});
+
+app.listen(5000, ()=>{
+  console.log("Server running on port 5000");
+});
